21f7442fb692bfcabc040b4ef445263d6a3a9cdf
abdo01-lab <abdelrahmanaboelnasrabdellatif@gmail.com>
2021-09-09 13:52:41 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java. util. Arrays;
+
+public class Main {
+
+        public static void main(String[] args) {
+            int[] array = {4, 6, 7, 1, 2, 3};
+            Arrays.sort(array);
+            for (int i = 0; i < array.length; i++) {
+
+                System.out.println(array[i]);
+            }
+        }
+    }
